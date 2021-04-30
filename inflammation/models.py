e0985ea0cd03518e55d0de05993bb872873d43fb
Kamilla Kopec-Harding <Kamilla.Kopec-Harding@manchester.ac.uk>
2021-04-30 10:51:41 +0100

    Coding style improvements

--- inflammation/models.py
+++ inflammation/models.py
@@ -15,17 +15,32 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2d inflammation data array."""
+    """Calculate the daily mean of a 2d inflammation data array.
+
+    :param data: 2d numpy data array (each column is a measurement occasion, each row
+    row is a day)
+    :returns: column-wise mean of data points
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2d inflammation data array."""
+    """Calculate the daily max of a 2d inflammation data array.
+
+    :param data: 2d numpy data array (each column is a measurement occasion,
+    each row is a day)
+    :returns: column-wise maximum of data points
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2d inflammation data array."""
+    """Calculate the daily min of a 2d inflammation data array.
+
+    :param data: 2d numpy data array (each column is a measurement occasion,
+    each row is a day)
+    :returns: column-wise minimum of data points
+    """
     return np.min(data, axis=0)
 
 
