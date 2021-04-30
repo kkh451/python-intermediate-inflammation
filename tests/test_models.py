830a83e4018db53c7beba41f6a9b1655327bc126
Kamilla Kopec-Harding <Kamilla.Kopec-Harding@manchester.ac.uk>
2021-04-30 11:24:25 +0100

    Add initial test cases for daily_max() and daily_min(), add parameterisation

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import numpy.testing as npt
-
+import pytest
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -31,3 +31,57 @@ def test_daily_mean_integers():
 
 
 # TODO(lesson-robust) Implement tests for the other statistical functions
+def test_daily_min_integers():
+    """Test that min function works for an array of positive integers."""
+    from inflammation.models import daily_min
+
+    test_array = np.array([[2, 7, 1],
+                           [4, 5, 2],
+                           [6, 3, 3]])  # yapf: disable
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(np.array([2, 3, 1]), daily_min(test_array))
+
+def test_daily_min_zeros():
+    """Test that min function works for an array of zeros."""
+    from inflammation.models import daily_min
+
+    # NB: the comment 'yapf: disable' disables automatic formatting using
+    # a tool called 'yapf' which we have used when creating this project
+    test_array = np.array([[0, 0],
+                           [0, 0],
+                           [0, 0]])  # yapf: disable
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(np.array([0, 0]), daily_min(test_array))
+
+def test_daily_max_integers():
+    """Test that max function works for an array of positive integers."""
+    from inflammation.models import daily_max
+
+    test_array = np.array([[9, 2, 1],
+                           [7, 4, 2],
+                           [5, 6, 3], ])  # yapf: disable
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(np.array([9, 6, 3]), daily_max(test_array))
+
+def test_daily_max_zeros():
+    """Test that max function works for an array of zeros."""
+    from inflammation.models import daily_max
+
+    # NB: the comment 'yapf: disable' disables automatic formatting using
+    # a tool called 'yapf' which we have used when creating this project
+    test_array = np.array([[0, 0],
+                           [0, 0],
+                           [0, 0]])  # yapf: disable
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(np.array([0, 0]), daily_max(test_array))
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
